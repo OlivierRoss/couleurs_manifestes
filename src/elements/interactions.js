1ddf9e4978b2bde16f17a56e363c43b144f3993d
Olivier Ross <olivier.ross@usherbrooke.ca>
2019-08-08 15:47:40 +0000

    comportement selection

--- src/elements/interactions.js
+++ src/elements/interactions.js
@@ -1,17 +1,39 @@
 require("../../sass/interactions.scss");
 
 export default {
+  //https://jqueryui.com/autocomplete/#combobox
   template: `
-    <footer class="flex footer-oeuvre">
-      <div><img src="/images/diese.svg" /></div>
-      <div v-on:click="oeuvre_aleatoire"><img src="/images/question.svg" /></div>
-      <div><img src="/images/partager.svg" /></div>
+    <footer class="footer-oeuvre">
+      <div v-if="mode == 'general'" class="flex menu-principal">
+        <div v-on:click="mode_selection"><img src="/images/diese.svg" /></div>
+        <div v-on:click="oeuvre_aleatoire"><img src="/images/question.svg" /></div>
+        <div><img src="/images/partager.svg" /></div>
+      </div>
+      <div v-if="mode == 'selection'" class="flex selection">
+        <div v-on:click="mode_general"><img src="/images/diese.svg" /></div>
+        <div class="input-container"><input v-on:change="selectionner" type="number" /></div>
+      </div>
+
     </footer>
   `,
+  data: function () {
+    return {
+      mode: "general"
+    }
+  },
 
   methods: {
     oeuvre_aleatoire: function () {
       this.$emit('update-oeuvre', null);
+    },
+    mode_general: function () {
+      this.mode = 'general';
+    },
+    mode_selection: function () {
+      this.mode = 'selection';
+    },
+    selectionner: function (event) {
+      this.$emit('update-oeuvre', event.target.value);
     }
   }
 };
