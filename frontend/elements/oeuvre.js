99fc41dc6a7ffae610b273c76a8d9cfd61d85799
Olivier Ross <olivier.ross@usherbrooke.ca>
2019-11-14 13:27:18 -0500

    styles generaux

--- frontend/elements/oeuvre.js
+++ frontend/elements/oeuvre.js
@@ -6,16 +6,27 @@ export default {
   template: `
     <section class="oeuvre">
       <header>
-        <div class="dimension-precedente" v-on:click="update_dimension" :data-id-dimension='infos.dimension_precedente.id'>{{ infos.dimension_precedente.nom }}</div>
-        <transition name="fade">
-          <div class="informations-oeuvre">
-            <h2>{{ infos.oeuvre.dimensions.titre.valeur || 'Nom' }}</h2>
-            <h3>{{ infos.oeuvre.dimensions.artiste.valeur || 'Artiste' }}</h3>
+        <div id="entete">
+          <div class="logo">
+            <img src="/images/Visuels/Autre/coma_icone-general.svg">
           </div>
-        </transition>
+          <div class="informations">
+            <h2 class="nom-oeuvre">{{ infos.oeuvre.dimensions.titre.valeur || 'Nom' }}</h2>
+            <h3 class="nom-artiste">{{ infos.oeuvre.dimensions.artiste.valeur || 'Artiste' }}</h3>
+          </div>
+        </div>
+        <div id="dimensions">
+          <div v-for="dimension in infos.oeuvre.dimensions" class="dimension" :data-id-dimension="dimension.id">{{ dimension.nom }}</div>
+        </div>
+        <!--
+        <div class="dimension-precedente" v-on:click="update_dimension" :data-id-dimension='infos.dimension_precedente.id'>{{ infos.dimension_precedente.nom }}</div>
         <div class="dimension-suivante" v-on:click="update_dimension" :data-id-dimension='infos.dimension_suivante.id'>{{ infos.dimension_suivante.nom }}</div>
+        -->
       </header>
-      <div class="contenu-dimension" v-touch:swipe="swipe">{{ infos.dimension_active.valeur }}</div>
+      <div class="contenu-dimension" v-touch:swipe="swipe">
+        <h2 class="nom-dimension">{{ infos.dimension_active.nom }}</h2>
+        <p class="valeur-dimension">{{ infos.dimension_active.valeur }}</p>
+      </div>
       <div class="affichage-liens"><div class="texte">Deux oeuvres similaires à découvrir :</div></div>
       <div class="liens flex">
         <div v-for="lien in infos.dimension_active.liens" v-on:click="update_oeuvre" :data-id-oeuvre="lien.id" class="lien"> {{ lien.titre }} </div>
