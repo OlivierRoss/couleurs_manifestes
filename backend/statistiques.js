ab48582f724a290508624c30d4bca3e7b52bd639
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-02-24 14:23:44 -0500

    statistiques alpha

--- backend/statistiques.js
+++ backend/statistiques.js
@@ -9,7 +9,6 @@ const Interaction = mongoose.model('Interaction');
 exports.log_interaction = (donnees) => {
   
   // TODO S'assurer qu'il n'y a pas de code malicieux
-  console.log(donnees);
   return new Promise ((resolve, reject) => {
     const interaction = new Interaction({
       session_id: donnees.session_id,
@@ -45,20 +44,31 @@ exports.get_statistiques = function () {
   return new Promise ((resolve, reject) => {
     get_interactions().then((interactions) => {
 
-      // Extraire information pertienente
-      interactions = _.map(interactions, (i) => {
-        var date_interaction = moment(i.timestamp);
-        return {
-          session_id: i.session_id,
-          oeuvre: i.oeuvre,
-          dimension: i.dimension,
-          heure: date_interaction.hour(),
-          day_of_week: date_interaction.day(),
-          day_of_year: date_interaction.dayOfYear()
-        }
-      });
+      var stats = {
+        sessions_quotidiennes: calculer_sessions_quotidiennes(interactions)
+      };
 
-      resolve(interactions);
+      resolve(stats);
     });
   });
 }
+
+///////// STATISTIQUES /////////////
+function calculer_sessions_quotidiennes (interactions) {
+
+  var statistique = { nom: "Interactions quotidiennes", dates: [], valeurs: [] };
+
+  // Regroupement
+  var hash_jour_sessions = _.countBy(interactions, (interaction) => {
+    return moment(interaction.timestamp).startOf('day').format();
+  });
+
+  // Mise en forme des donnees
+  for(var jour in hash_jour_sessions) {
+    statistique.dates.push(jour);
+    statistique.valeurs.push(hash_jour_sessions[jour]);
+  }
+  
+  return statistique;
+}
+
--- frontend/statistiques.js
+++ frontend/statistiques.js
@@ -2,9 +2,37 @@ function afficher_stats () {
   new Vue({
     el: '#container-partager',
     template: `
-      <div id="container-partager">
-        Statistiques!!!
-      </div>`
+    <div id="container-partager">
+    <div class="chart-container" style="position: relative; height:400px; width:50%">
+      <canvas id="myChart" width="100" height="100"></canvas>
+    </div>
+    </div>`,
+    mounted: function () {
+      this.interactions = window.interactions;
+
+      var ctx = document.getElementById('myChart');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: this.interactions.sessions_quotidiennes.dates,
+          datasets: [{
+            fill: false,
+            label: this.interactions.sessions_quotidiennes.nom,
+            data: this.interactions.sessions_quotidiennes.valeurs
+          }]
+        },
+        options: {
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day'
+              }
+            }]
+          }
+        }
+      });
+    }
   });
 }
 
