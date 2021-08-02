580d1e3da9d1f1635797128f4eca525d3cdd34fa
DCARTH <83974382+DCARTH@users.noreply.github.com>
2021-08-02 11:57:39 -0400

    installer express-force-ssl

--- app.js
+++ app.js
@@ -62,5 +62,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.use(function (req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+);
 module.exports = app;
